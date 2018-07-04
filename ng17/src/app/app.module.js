ffe195fd35a95f33b52419611a03022faa00576b
Lalit Behera <lalit143.er@gmail.com>
2018-07-04 23:23:41 +0530

    angular folder structure added

---
+++ ng17/src/app/app.module.js
@@ -0,0 +1,5 @@
+// Define the `phonecatApp` module
+angular.module('ngApp', [
+    // ...which depends on the `phoneList` module
+    'navigation'
+  ]);
---
+++ ng17/src/app/navigation/navigation.component.js
@@ -0,0 +1,6 @@
+
+// Register the `phoneList` component on the `phoneList` module,
+angular.module('navigation').component('navigation', {
+  templateUrl: 'phone-list/phone-list.template.html',
+  controller:''
+});
