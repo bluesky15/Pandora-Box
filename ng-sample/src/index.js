4772f1158d337248d90d9c798572d519eb09515b
lkb <lalitkbehera@gmail.com>
2018-07-06 18:57:08 +0530

    ng-sample angularjs routing demo

--- ng-sample/src/index.js
+++ ng-sample/src/index.js
@@ -1,12 +1,22 @@
 import angular from 'angular';
 import './route.min.js';
 
-angular.module("myApp", [])
-    .controller("myController", function ($scope, $http) {
-        var names = ['John', 'Jessie', 'Johanna', 'Joy', 'Mary', 'Peter', 'Sebastian', 'Erika',
-            'Patrick', 'Samantha'
-        ];
-        $scope.names = names;
+// angular.module("myApp", [])
+//     .controller("myController", function ($scope, $http) {
+//         var names = ['John', 'Jessie', 'Johanna', 'Joy', 'Mary', 'Peter', 'Sebastian', 'Erika',
+//             'Patrick', 'Samantha'
+//         ];
+//         $scope.names = names;
 
+//     });
+// route.$http;
+var app = angular.module("myApp", ['ngRoute']);
+app.config(function ($routeProvider) {
+    $routeProvider.when('/', {
+        template: 'welcome user!'
+    }).when('/another', {
+        template: 'welcome user, again!'
+    }).otherwise({
+        redirectTo: '/'
     });
-route.$http;
+});
